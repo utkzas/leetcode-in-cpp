7e14e07b6e8fb66120a801ecefecd412dad80625
Utkarsh Sharma <46284927+utkzas@users.noreply.github.com>
2022-01-17 22:52:57 +0530

    Time: 342 ms (26.19%), Space: 64.5 MB (32.61%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,18 +1,21 @@
+/*
+the logic behind solution is straight-forward, for each possible combination of two or more (given) array elements check if
+1. all the elements are unique.
+2. the length is longest
+*/
 class Solution {
     int len{0};
-public:
+public: 
     int maxLength(vector<string>& arr) {
         checkLen("", arr, 0);
         return len;
     }
-    
     void checkLen(string str, vector<string> &arr, int itr) {
         if (!isUnique(str)) return;
         if (str.size() > len) len = str.size();
         for (int i = itr; i < arr.size(); i++)
             checkLen(str+arr[i], arr, i+1);
     }
-    
     bool isUnique(string word) {
         set<char> st;
         for (auto ele : word) {
