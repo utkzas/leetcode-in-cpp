2df0d4160e97a2b8a50fae3aed43eaa43073d480
Utkarsh Sharma <46284927+utkzas@users.noreply.github.com>
2022-05-22 18:51:05 +0530

    Time: 30 ms (14.92%), Space: 24.1 MB (62.48%) - LeetHub

---
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+/*
+Brute Force: Do any dfs traversal, store all the nodes in a vector, and then sort it. now get the kth element
+TC: O(N)+O(NlogN) // to traverse + sort the vector
+SC: O(N) // to store entire BST into the vector
+
+Optimised: 
+inorder of a bst gives elements in sorted order, then we can use a counter till k.
+
+TC:
+Iterative and recursive traversal will give TC: O(N) SC: O(N)
+But Morris Treversal will give TC:(N) SC: O(1)
+*/
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int &k) { // added a & here. but why?
+        if(root==NULL) return -1;
+        
+        int left=kthSmallest(root->left,k); // left call
+        if(left!=-1) return left; // left has the ans? return it!
+        k--;
+        if(k==0) return root->val; // this is the ans! the kth smallest! 
+        
+        int right=kthSmallest(root->right,k); // right call
+        if(right!=-1) return right; // right has the answ? return it!
+        
+        return -1; // no answer from this call ;( return -1
+    }
+};
